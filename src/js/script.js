fa4b9bb754323096543c306eab293091a56d8c56
Iván Albizu <ivan.albizu@gmail.com>
2021-12-06 19:15:59 +0100

    Update JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,7 @@
-import * as dat from 'dat.gui';
+import * as dat from 'dat.gui'
+const gui = new dat.GUI()
 
-const gui = new dat.GUI();
-const banner = document.querySelector('.banner')
-const w = {
+const settings = {
   'width': 1200,
   '--g': 20,
   '--pi-1-x': 36,
@@ -11,22 +10,22 @@ const w = {
   '--pi-4-x': 22
 }
 
-window.onload = function() {
-  const pe1 = getComputedStyle(banner).getPropertyValue('--pe-1');
-  console.log('pe1 :>> ', pe1);
-  gui.add(w, 'width', 600, 1200).onChange(setValue);
-  gui.add(w, '--g', 0, 30).onChange(setValue);
-  gui.add(w, '--pi-1-x', 0, 50).onChange(setValue);
-  gui.add(w, '--pi-2-x', 55, 99).onChange(setValue);
-  gui.add(w, '--pi-3-x', 56, 99).onChange(setValue);
-  gui.add(w, '--pi-4-x', 0, 48).onChange(setValue);
-};
-
-function setValue() {
-  banner.style.width = w.width+"px";
-  banner.style.setProperty('--g', w['--g']+"px");
-  banner.style.setProperty('--pi-1-x', w['--pi-1-x']+"%");
-  banner.style.setProperty('--pi-2-x', w['--pi-2-x']+"%");
-  banner.style.setProperty('--pi-3-x', w['--pi-3-x']+"%");
-  banner.style.setProperty('--pi-4-x', w['--pi-4-x']+"%");
+const setValue = banner => {
+  banner.style.width = settings.width+"px"
+  banner.style.setProperty('--g', settings['--g']+"px")
+  banner.style.setProperty('--pi-1-x', settings['--pi-1-x']+"%")
+  banner.style.setProperty('--pi-2-x', settings['--pi-2-x']+"%")
+  banner.style.setProperty('--pi-3-x', settings['--pi-3-x']+"%")
+  banner.style.setProperty('--pi-4-x', settings['--pi-4-x']+"%")
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+  const banner = document.querySelector('.banner')
+
+  gui.add(settings, 'width', 600, 1200).onChange(setValue.bind(null, banner))
+  gui.add(settings, '--g', 0, 30).onChange(setValue.bind(null, banner))
+  gui.add(settings, '--pi-1-x', 0, 50).onChange(setValue.bind(null, banner))
+  gui.add(settings, '--pi-2-x', 55, 99).onChange(setValue.bind(null, banner))
+  gui.add(settings, '--pi-3-x', 56, 99).onChange(setValue.bind(null, banner))
+  gui.add(settings, '--pi-4-x', 0, 48).onChange(setValue.bind(null, banner))
+})
